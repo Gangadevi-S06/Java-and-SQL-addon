3fe477fd8a20140489166079bd597b0acc7769c8
Gangadevi-S06 <153528890+Gangadevi-S06@users.noreply.github.com>
2025-06-05 14:50:00 +0530

    Create Task3.java

---
+++ Task3.java
@@ -0,0 +1,16 @@
+package Basics;
+
+public class sample {
+
+	public static void main(String[] args) {
+	
+		int Principal=1000;
+		int Rate=5;
+		int Time=2;
+		int mul=Principal*Rate*Time;  //mul=p*r*t
+		int div=mul/100;
+		System.out.println("Your Interest is:");
+		System.out.println(div);
+	}
+
+}
