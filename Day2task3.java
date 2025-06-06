4ad1d0d0cc11296d54cbdc74a6b46424173075f5
Gangadevi-S06 <153528890+Gangadevi-S06@users.noreply.github.com>
2025-06-06 11:27:11 +0530

    Create Day2task3.java

---
+++ Day2task3.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("Enter your Name:");
+        String a=s.next();
+        System.out.println("Enter your Mobile Number:");
+        int b=s.nextInt();
+        System.out.println("Enter your Email ID:");
+        String c=s.next();
+        System.out.println("Enter your Address:");
+        String d=s.next();
+        System.out.println("Enter your Skillset:");
+        String e=s.next();
+        System.out.println("Enter your Project Description:");
+        String f=s.next();
+        System.out.println("My Name is:"+a);
+        System.out.println("My Mobile Number is:"+b);
+        System.out.println("My Email ID is:"+c);
+        System.out.println("My Address is:"+d);
+        System.out.println("My Skillset is:"+e);
+        System.out.println("My Project Description is:"+f);
+        
+    }
+}
