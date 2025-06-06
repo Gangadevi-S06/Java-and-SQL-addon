00fd26978cd2ca7ac8bc1cef47ed4b390ac75a7e
Gangadevi-S06 <153528890+Gangadevi-S06@users.noreply.github.com>
2025-06-06 12:39:09 +0530

    Create Day2task1.java

---
+++ Day2task1.java
@@ -0,0 +1,20 @@
+package programs;
+
+public class sumofeven {
+
+	public static void main(String[] args) {
+		//finding sum of even numbers
+		int[] number= {1,2,3,4,5,6,7,8,9,10,11,12,13,14};
+	    int sum=0;
+		for(int i=0;i<=number.length-1;i++) {
+			if(number[i]%2==0) {
+				System.out.println(number[i]);
+			}
+			
+		}
+		
+		
+	
+	}
+}	
+		
