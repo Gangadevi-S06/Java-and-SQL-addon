89fc1e7ec3a99239fb0c698b605d28a9daa2470f
Gangadevi-S06 <153528890+Gangadevi-S06@users.noreply.github.com>
2025-06-06 10:42:57 +0530

    Create Day2task1.java

---
+++ Day2task1.java
@@ -0,0 +1,16 @@
+package programs;
+
+public class demomethod {
+
+	public static void Details(String name,String Job,String Package) {
+		System.out.println("My name is "+name+ "My Job is "+Job+ "My Package is "+Package);
+			}
+	
+	public static void main(String[] args) {
+		Details("Gangadevi","Web developer","3LPA");
+		Details("Devika","Data Analyst","3LPA");
+	
+		
+	}
+
+}
